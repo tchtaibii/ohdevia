eaa50bd9c5126eecf5cea42cbef037f43108a847
Taha Chtaibi <tchtaibi@e1r3p4.1337.ma>
2022-08-25 04:25:21 +0100

    store list

--- asset/script/store_list.js
+++ asset/script/store_list.js
@@ -4,13 +4,13 @@ function displaynone(value, imgg){
     console.log(imgg);
     console.log(x.style);
     if (x.style.visibility === "collapse") {
-        y.src = "arrowu.svg";
+        y.src = "asset/img/arrowu.svg";
         x.style.visibility = "visible";
         x.style.opacity = 1;
         x.style.transition = "visibility 0s linear 0s, opacity 700ms";
 
     } else {
-        y.src = "arrowd.svg";
+        y.src = "asset/img/arrowd.svg";
         x.style.visibility = "collapse";
         x.style.opacity = 0;
         x.style.transition = "visibility 0s linear 700ms, opacity 700ms";
