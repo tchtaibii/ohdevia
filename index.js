a80cefbdcabd62e719a0291f812298b3eb9b7540
Taha Chtaibi <tchtaibi@e1r3p4.1337.ma>
2022-08-25 04:03:59 +0100

    clean

--- index.js
+++ index.js
@@ -6,9 +6,13 @@ function displaynone(value, imgg){
     if (x.style.visibility === "collapse") {
         y.src = "arrowu.svg";
         x.style.visibility = "visible";
+        x.style.opacity = 1;
+        x.style.transition = "visibility 0s linear 0s, opacity 700ms";
 
     } else {
         y.src = "arrowd.svg";
-      x.style.visibility = "collapse";
+        x.style.visibility = "collapse";
+        x.style.opacity = 0;
+        x.style.transition = "visibility 0s linear 700ms, opacity 700ms";
     }
   }
