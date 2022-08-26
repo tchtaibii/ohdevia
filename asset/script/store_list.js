2af977c30173f7e6e26aa5f1b37e8f09dbeb5757
TEHSUS <93588863+tchtaibii@users.noreply.github.com>
2022-08-26 09:07:18 +0100

    .

--- asset/script/store_list.js
+++ asset/script/store_list.js
@@ -1,18 +1,16 @@
 function displaynone(value, imgg){
     var y = document.getElementById(imgg);
     var x = document.getElementById(value);
-    console.log(imgg);
-    console.log(x.style);
     if (x.style.visibility === "collapse") {
         y.src = "asset/img/arrowu.svg";
         x.style.visibility = "visible";
         x.style.opacity = 1;
-        x.style.transition = "visibility 0s linear 0s, opacity 700ms";
+        x.style.transition = "visibility 0s linear 0s, opacity 500ms";
 
     } else {
         y.src = "asset/img/arrowd.svg";
         x.style.visibility = "collapse";
         x.style.opacity = 0;
-        x.style.transition = "visibility 0s linear 700ms, opacity 700ms";
+        x.style.transition = "visibility 0s linear 500ms, opacity 500ms";
     }
   }
